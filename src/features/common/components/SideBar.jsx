ee0abd1226e81fd3b854fed7a68d77aa8731c682
SugamChaudharry <nextgenachieverss@gmail.com>
2024-11-26 04:42:58 +0530

    add links to sidebar

--- src/features/common/components/SideBar.jsx
+++ src/features/common/components/SideBar.jsx
@@ -9,26 +9,32 @@ function SideBar() {
     {
       iconComponent: <IoHomeOutline className="w-full h-full" />,
       name: "Home",
+      path: "/"
     },
     {
       iconComponent: <AiOutlineLike className="w-full h-full" />,
       name: "Liked Videos",
+      path: "/"
     },
     {
       iconComponent: <AiOutlineHistory className="w-full h-full"/>,
       name: "History",
+      path: "/"
     },
     {
       iconComponent: <IoVideocamOutline className="w-full h-full" />,
       name: "My Content",
+      path: "/"
     },
     {
       iconComponent: <IoFolderOpenOutline className="w-full h-full" />,
       name: "Collections",
+      path: "/"
     },
     {
       iconComponent: <RiUserHeartLine className="w-full h-full" />,
       name: "Subscribers",
+      path: "/"
     },
   ]
   const itemsList2 = [
@@ -47,10 +53,10 @@ function SideBar() {
     <div className="fixed w-[20%] h-[90%] mt-[80px] border-r border-white flex flex-col justify-between">
       <div className=" px-2 flex flex-col gap-2 mt-2">
         {itemsList1.map((e,i) => (
-          <div key={i} className="border text-white flex gap-2 p-2">
+          <Link to={e.path} key={i} className="border text-white flex gap-2 p-2">
             <span className="size-5">{e.iconComponent}</span>
             <span>{e.name}</span>
-          </div>
+          </Link>
         ))}
       </div>
       <div className="p-2 flex flex-col gap-2">
