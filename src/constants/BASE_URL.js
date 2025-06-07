f040dceb77297b06cc22a34103e4720e5cafb79a
aryan khandelwal <aryankhandelwal15@gmail.com>
2025-06-07 15:34:37 +0530

    diff env file for prod and dev

--- src/constants/BASE_URL.js
+++ src/constants/BASE_URL.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = import.meta.env.VITE_BASE_URL_LOCAL_HOST;
-export const BASE_URL = import.meta.env.VITE_BASE_URL_PRODUCTION;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+// export const BASE_URL = import.meta.env.VITE_BASE_URL_PRODUCTION;
